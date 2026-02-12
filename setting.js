f7e5d478b566c65434080805630998539d2eccb4
Ali <frhn123786@gmail.com>
2026-02-12 14:56:29 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,17 @@
+const settings = {
+  packname: '𓆩 𝗔𝗽𝗲𝘅-𝗠𝗗 𓆪',
+  author: '‎',
+  botName: "𓆩 𝗔𝗽𝗲𝘅-𝗠𝗗 𓆪",
+  botOwner: '𝗔𝗽𝗲𝘅 𝗣𝗿𝗲𝗱𝗮𝘁𝗼𝗿', // Your name
+  ownerNumber: '256741897733', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/frhn123786-hash/Apex-MD/tree/main",
+};
+
+module.exports = settings;
+
